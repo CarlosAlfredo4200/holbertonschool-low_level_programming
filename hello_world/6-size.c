9829074b7ec469a8546a228ab34780e1a3c42b8b
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-02-15 18:51:57 -0500

    Update 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,10 +12,10 @@ int entero;
 long int longEntero;
 long long int longLentero;
 float flotante;
-printf("Size of a char : %lu byte(s)\n", (unsigned long)sizeof(carater));
-printf("Size of a int : %lu byte(s)\n", (unsigned long)sizeof(entero));
-printf("Size of a long int : %lu byte(s)\n", (unsigned long)sizeof(longEntero));
-printf("Size of a long long int : %lu byte(s)\n", (unsigned long)sizeof(longLentero));
-printf("Size of a float : %lu byte(s)\n", (unsigned long)sizeof(flotante));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(carater));
+printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(entero));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(longEntero));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(longLentero));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(flotante));
 return (0);
 }
