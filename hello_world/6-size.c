49b412c330ce4bbe429bff6d00012758382bfd94
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-02-15 20:04:36 -0500

    Update 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *main - a C program that prints the size of various types on the computer 
+ *main - a C program that prints the size of various types on the computer it is compiled and run on 
  *Return: 0 (Success)
  */
 int main(void)
@@ -8,12 +8,12 @@ int main(void)
 char carater;
 int entero;
 long int longEntero;
-long long int llEntero;
+long long int llEnt;
 float flotante;
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(carater));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(entero));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(longEntero));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(llEntero));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(llEnt));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(flotante));
 return (0);
 }
