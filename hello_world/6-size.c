fd50c3340dbe1f2975f98654096be3e805db7080
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-02-15 20:07:28 -0500

    Update 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-*main -C program that prints the size of various types on the computer it is compiled and run on 
+*main -C program that prints the size of various types on the computer it is compiled an 
 *Return: 0 (Success)
 */
 int main(void)
