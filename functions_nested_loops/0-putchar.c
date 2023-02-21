4b3985d60a1f99fdbe603f0d03a790eb23f1ae21
CarlosAlfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-02-21 10:14:18 -0800

    0-putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <time.h>
 #include <ctype.h>
-#include <main.h>
 /* more headers goes there */
 /* betty style doc for function main goes there */
 /**
@@ -13,14 +12,14 @@
 */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
 }
