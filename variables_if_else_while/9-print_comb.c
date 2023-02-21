618f7cf203afbd9a86902c8fa4fe8e35d7269f77
CarlosAlfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-02-21 05:59:00 -0800

    9-print_comb

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <ctype.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+*main - Entry point
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+int x;
+for (x = 0; x < 10; x++)
+{
+putchar(x + '0');
+if (x < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
