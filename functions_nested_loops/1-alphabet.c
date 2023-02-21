13930b5f6f8bc8672a3e65bdd37bb35a201a9941
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-02-21 15:24:15 -0500

    Update 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 /* more headers goes there */
 /* betty style doc for function main goes there */
 /**
- *print - alfhabet
+ *print_alphabet - unction that prints the alphabet, in lowercase
  *
  *Return: Always 0 (success)
  */
