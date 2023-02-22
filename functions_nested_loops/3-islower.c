19350431090d454d6af962ef0026240830f516a5
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-02-22 08:54:53 -0500

    Update 3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,10 +1,8 @@
 #include "main.h"
-/* more headers goes there */
-/* betty style doc for function main goes there */
 /**
-*_islower - Write a function that checks for lowercase character.
-*c: single letter input
-*Return: 1 if int c is lowercase, 0 if otherwise
+* _islower - a function that checks for lowercase character
+* @c: single letter input
+* Return: 1 if int c is lowercase, 0 if otherwise
 */
 int _islower(int c)
 {
