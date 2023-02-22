ebfa81f6f3c45661ac8db6b46b2a881143643584
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-02-22 08:41:34 -0500

    Update 3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,18 +2,13 @@
 /* more headers goes there */
 /* betty style doc for function main goes there */
 /**
- *_islower - Write a function that checks for lowercase character.
- *Return: Always 0 (success)
- */
-
+*_islower - Write a function that checks for lowercase character.
+*Return: Always 0 (success)
+*/
 int _islower(int c)
 {
-if (_islower(c))
-{
+if (c >= 'a' && c <= 'z')
 return (1);
-}
 else
-{
 return (0);
-}
 }
