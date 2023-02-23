8302fe2457de5794acdb22004b15a33d6c760c41
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-02-23 18:50:32 -0500

    Update 6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -9,10 +9,12 @@ int i;
         for (i = 0; i < n; i++)
         {
             _putchar('_');
-     _putchar('\n');
+     
         }
+    
         else
-        
-        
+        {
+             _putchar('\n');
+        }
     _putchar('\n');
 }
