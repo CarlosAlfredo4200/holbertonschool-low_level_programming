b5077f5d9d57c9d5c4379cda077369958f181a29
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-02-23 18:57:33 -0500

    Update 6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -5,19 +5,17 @@
  */
 void print_line(int n)
 {
-    int i;
-    if (n > 0)
-    {
-        for (i = 0; i < n; i++)
-        {
-            _putchar('_');
-        }
-        _putchar('\n');
-    }
-
-    else
-    {
-        _putchar('\n');
-    }
-    
+int i;
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+else
+{
+_putchar('\n');
+}    
 }
