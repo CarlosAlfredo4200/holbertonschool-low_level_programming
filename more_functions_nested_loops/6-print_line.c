8e544bd80ce668214aff2b7322b1508a0235d507
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-02-23 18:53:08 -0500

    Update 6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -3,16 +3,19 @@
  * print_line - a function that draws a straight line in the terminal
  * Return: 0
  */
-void print_line(int n){
-int i;
+void print_line(int n)
+{
+    int i;
     if (n > 0)
+    {    
         for (i = 0; i < n; i++)
         {
             _putchar('_');
-     
         }
-    
-        else
+      _putchar('\n');   
+    }
+         
+     else
         {
              _putchar('\n');
         }
