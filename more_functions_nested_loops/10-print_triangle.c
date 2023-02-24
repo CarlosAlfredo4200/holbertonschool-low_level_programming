ba8e81a6117615387b224bb535b1f5b8336f9ca1
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-02-24 09:44:16 -0500

    Update 10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -4,17 +4,21 @@
 *@size: the size of the square triangle
 *Return: empty
 */
-void print_triangle(int size){
+void print_triangle (int size){
 int a, b, c;
-if (size <= 0){
+if (size <= 0)
+{
 _putchar('\n');
 }
-else{
+else
+{
 for (a = 0; a < size; a++){
-for (b = size - a; y > 1; b--){
+for (b = size - a; y > 1; b--)
+{
 _putchar(46);
 }
-for (c = 0; c <= a; c++){
+for (c = 0; c <= a; c++)
+{
 _putchar(35);
 }
 _putchar('\n');
