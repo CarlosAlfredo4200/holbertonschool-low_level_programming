94c09f077ebe44c21912ec96630852b0b899fad5
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-02-24 08:30:47 -0500

    Update 9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,37 +1,36 @@
 #include <stdio.h>
 /**
- * main - main block, solve fizz buzz from numbers 1 to 100
- * Description: Multiples of 3, print Fizz. Multiples of 5, print Buzz.
- * Multiples of both 3 and 5 should print FizzBuzz.
- * Return: 0
- */
+* main - main block, solve fizz buzz from numbers 1 to 100
+* Description: Multiples of 3, print Fizz. Multiples of 5, print Buzz.
+* Multiples of both 3 and 5 should print FizzBuzz.
+* Return: 0
+*/
 int main(void)
 {
-    int k;
-     
-    for (k = 1; k <= 100; k++)
-    {
-        if (k % 3 == 0 && k % 5 == 0)
-        {
-            printf("FizzBuzz");
-        }
-        else if (k % 3 == 0)
-        {
-            printf("Fizz");
-        }
-        else if (k % 5 == 0)
-        {
-            printf("Buzz");
-        }
-        else
-        {
-            printf("%d", k);
-        }
-        if ( k !=100)
-        {
-        printf(" ");
-        }
-    }
-    printf("\n");
-    return (0);
+int k;
+for (k = 1; k <= 100; k++)
+{
+if (k % 3 == 0 && k % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (k % 3 == 0)
+{
+printf("Fizz");
+}
+else if (k % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", k);
+}
+if ( k !=100)
+{
+printf(" ");
+}
+}
+printf("\n");
+return (0);
 }
