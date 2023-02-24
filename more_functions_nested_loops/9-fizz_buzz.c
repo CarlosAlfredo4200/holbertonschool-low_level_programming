83685fd53d0ac4bc04541ab9ef09d2cb82486350
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-02-23 21:29:59 -0500

    Update 9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,44 +10,28 @@
  * Multiples of both 3 and 5 should print FizzBuzz.
  * Return: 0
  */
-int main(void)
-    
+
+int main()
 {
-    
     int k;
-    for (k = 0; k <= 15; k++)
+    for (k = 1; k <= 15; k++)
     {
-	if (k % 3 == 0 && k % 5 == 0)
-	    
-	{
-	    
-	    printf("FizzBuzz\n");
-	    
-	}
-	
-	else if (k % 3 == 0)
-	    
-	{
-	    
-	    printf("Fizz\n");
-	    
-	}
-	
-	else if (k % 5 == 0)
-	    
-	{
-	    
-	    printf("Buzz\n");
-	    
-	}
-	
-	else
-	    
-	{
-	    
-	    printf("%d\n", k);
-	    
-	}
-	
-    
+        if (k % 3 == 0 && k % 5 == 0)
+        {
+            printf("FizzBuzz\n");
+        }
+        else if (k % 3 == 0)
+        {
+            printf("Fizz\n");
+        }
+        else if (k % 5 == 0)
+        {
+            printf("Buzz\n");
+        }
+        else
+        {
+            printf("%d\n", k);
+        }
+    }
+    return (0);
 }
