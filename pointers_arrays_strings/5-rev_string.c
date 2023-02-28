22215cfedb88da4b89ec1cb56482eb7cd2558562
CarlosAlfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-02-27 16:19:53 -0800

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - a function that prints a string, in reverse, followed by a new
+ *
+ * @s: This is char
+ *
+ */
+void rev_string(char *s)
+{
+int k = 0;
+int len = 0;
+char aux;
+while (s[k++])
+{
+len++;
+for (k = len - 1; k >= len / 2; k--)
+{
+aux = s[k];
+s[k] = s[len - k - 1];
+s[len - k - 1] = aux;
+}
+}
+}
