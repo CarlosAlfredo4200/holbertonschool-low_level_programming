f19cf5f642df4da946d2f28575bf58612e164a64
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-02-28 11:00:25 -0500

    Update 8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -8,17 +8,16 @@
  *
  *
  */
-
 void print_array(int *a, int n)
 {
-    int index;
-    for (index = 0; index < n; index++)
-    {
-	printf("%d", a[index]);
-	if (index != (n - 1))
-	{
-	    printf(", ");
-	}
-    }
-    putchar('\n');
+int k;
+for (k = 0; k < n; k++)
+{
+printf("%d", a[k]);
+if (index != (n - 1))
+{
+printf(", ");
+}
+}
+_putchar('\n');
 }
