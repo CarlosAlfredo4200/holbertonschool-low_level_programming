95ad003c3393e0fe2bddd27f5349a12ba0d55d38
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-03-01 12:08:25 -0500

    Update 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -5,15 +5,12 @@
  * Return: void.
  */
 int _atoi(char *s)
-{
-    
+{ 
     char num_str[20];
     int num;
     int i, j, k = 0;
     int numero = 0;
     int signo = 1;
-
-    // Buscar el inicio del número
     for (i = 0; s[i] != '\0'; i++)
     {
         if (s[i] >= '0' && s[i] <= '9')
@@ -22,7 +19,6 @@ int _atoi(char *s)
         }
     }
 
-    // Copiar el número en una cadena de caracteres separada
     for (j = 0; s[j] != '\0'; j++)
     {
         if (s[i + j] >= '0' && s[i + j] <= '9')
@@ -41,21 +37,11 @@ int _atoi(char *s)
         i = 1;
     }
 
-
-    // Convertir la cadena de caracteres a un entero
-
     for (; num_str[k] != '\0'; k++)
     {
         numero = numero * 10 +  num_str[k] - '0';
-        
-        
-
-         
     }
     numero *= signo;
         printf("El número es: %d\n", numero);
-     
-
-
     return 0;
 }
