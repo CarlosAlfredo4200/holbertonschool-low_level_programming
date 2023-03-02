6c0a25b2c134ec11cdde7fa1f3e91277add30aa2
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-03-02 14:13:47 -0500

    Update 6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 /**
 * cap_string - Write a function that capitalizes all words of a string.
 *
-* @entry: This is the input string
+* @cadena: This is the input string
 *
 * Return: String capitalized
 */
