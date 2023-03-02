2a5db8506a325964ca212baecb5c103cc8061d29
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-03-02 14:12:53 -0500

    Update 6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *cadena)
 int i = 0;
 while (cadena[i])
 {
-while (!(cadena[i] >= 'a' && cadena[i] <='z'))
+while (!(cadena[i] >= 'a' && cadena[i] <= 'z'))
 i++;
 if (
 cadena[i - 1] == ' ' ||
@@ -27,9 +27,9 @@ cadena[i - 1] == '(' ||
 cadena[i - 1] == ')' ||
 cadena[i - 1] == '{' ||
 cadena[i - 1] == '}' ||
-i == 0 )
-cadena[i] -=32;
+i == 0)
+cadena[i] -= 32;
 i++;
 }
-return cadena;
+return (cadena);
 }
