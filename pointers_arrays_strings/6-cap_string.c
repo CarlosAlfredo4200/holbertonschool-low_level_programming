1bcbb59eac11e360a6f8117df203efa18a538580
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-03-02 10:58:39 -0500

    Update 6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,7 @@
 char *cap_string(char *cadena)
 {
 int i;
-if(cadena[0]>=97&&cadena[0]<=122)
+if(cadena[0] >= 97 && cadena[0] <= 122)
 {
 cadena[0]-=32;
 }
@@ -17,7 +17,7 @@ for(i=1;cadena[i]!='\0';i++)
 {
 if(cadena[i-1]==32)
 {
-if(cadena[i]>=97 &&cadena[i]<=122)
+if(cadena[i] >=97 && cadena[i] <= 122)
 {
 cadena[i]-=32;
 }
