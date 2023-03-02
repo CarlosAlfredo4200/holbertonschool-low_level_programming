7b786d29be0133bb4f2f9fa6a0ee8ad896a0689d
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-03-01 22:03:44 -0500

    Update 3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,24 +8,24 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-    int res = 0;
-    int countS1 = 0;
-    int countS2 = 0;
-    for (int k = 0; s1[k] != '\0'; k++)
-    {
-	countS1++;
-    }
-    for (int k = 0; s2[k] != '\0'; k++)
-    {
-	countS2++;
-    }
-    if (countS1 < countS2)
-    {
-	res = -15;
-    }
-    else if (countS2 < countS1)
-    {
-	res = 15;
-    }
-    return (res);
+int res = 0;
+int countS1 = 0;
+int countS2 = 0;
+for (int k = 0; s1[k] != '\0'; k++)
+{
+countS1++;
+}
+for (int k = 0; s2[k] != '\0'; k++)
+{
+countS2++;
+}
+if (countS1 < countS2)
+{
+res = -15;
+}
+else if (countS2 < countS1)
+{
+res = 15;
+}
+return (res);
 }
