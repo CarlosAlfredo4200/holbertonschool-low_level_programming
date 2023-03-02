2c786cd8274bbf1f038848ef23ff4d700ac40a3d
CarlosAlfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-03-01 19:00:38 -0800

    3-strcmp.c

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strcmp - a function that compares two strings
+ * @dest: This is the output dest
+ * @src: This is the input source
+ *
+ *Return: this is res
+ */
+int _strcmp(char *s1, char *s2)
+{
+    int res = 0;
+    int countS1 = 0;
+    int countS2 = 0;
+    for (int k = 0; s1[k] != '\0'; k++)
+    {
+	countS1++;
+    }
+    for (int k = 0; s2[k] != '\0'; k++)
+    {
+	countS2++;
+    }
+    if (countS1 < countS2)
+    {
+	res = -15;
+    }
+    else if (countS2 < countS1)
+    {
+	res = 15;
+    }
+    return (res);
+}
