389a2b0c5e66ccfd9dc5faee6cfdd7461969e7aa
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-03-03 15:25:01 -0500

    Update 3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -6,16 +6,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-int i, k;
-int count = -1;
+int i, k, existe;
+int count = 0;
 for (i = 0; s[i] != '\0'; i++)
 {
+existe = 0;
 for (k = 0; accept[k] != '\0'; k++)
 {
-if (s[i] == accept[k - 1])
+if (accept[k] == s[i])
 {
 count++;
+existe = 1;
 }
+}
+if (existe == 0)
+{
+return (count);
 }
 }
 return (count);
