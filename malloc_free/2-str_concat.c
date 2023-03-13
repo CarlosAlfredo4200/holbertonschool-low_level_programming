f2f843752f890e9c2980a782848c0f968833ac8a
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-03-13 15:25:51 -0500

    Update 2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -8,33 +8,30 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *conct;
-    int i, k;
-    if (s1 == 0)
-        s1 = "";
-    if (s2 == 0)
-        s2 = "";
-
-    for (i = 0; s1[i] != '\0'; i++)
-		;
-	for (k = 0; s2[k] != '\0'; k++)
-		;
-	k++;
-   conct = malloc(i * sizeof(*s1) + k * sizeof(*s2));
-    if (conct == NULL)
-        return (NULL);
-
-    i = k = 0;
-
-    for (i = 0; s1[i] != '\0'; i++)
-    {
-        conct[i] = s1[i];
-    }
-    for (k = 0; s2[k] != '\0'; k++)
-    {
-        conct[i] = s2[k];
-        i++;
-    }
-    conct[i] = '\0';
-    return (conct);
+char *conct;
+int i, k;
+if (s1 == 0)
+s1 = "";
+if (s2 == 0)
+s2 = "";
+for (i = 0; s1[i] != '\0'; i++)
+;
+for (k = 0; s2[k] != '\0'; k++)
+;
+k++;
+conct = malloc(i * sizeof(*s1) + k * sizeof(*s2));
+if (conct == NULL)
+return (NULL);
+i = k = 0;
+for (i = 0; s1[i] != '\0'; i++)
+{
+conct[i] = s1[i];
+}
+for (k = 0; s2[k] != '\0'; k++)
+{
+conct[i] = s2[k];
+i++;
+}
+conct[i] = '\0';
+return (conct);
 }
