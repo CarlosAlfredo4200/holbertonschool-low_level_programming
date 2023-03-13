f50a68192b0d84b8506deaa26a4046281b57fcdd
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-03-13 15:20:31 -0500

    Update 2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -20,11 +20,11 @@ char *str_concat(char *s1, char *s2)
 	for (k = 0; s2[k] != '\0'; k++)
 		;
 	k++;
-    i = k = 0;
-    conct = malloc(i * sizeof(*s1) + k * sizeof(*s2));
+   conct = malloc(i * sizeof(*s1) + k * sizeof(*s2));
     if (conct == NULL)
         return (NULL);
- 
+
+    i = k = 0;
 
     for (i = 0; s1[i] != '\0'; i++)
     {
