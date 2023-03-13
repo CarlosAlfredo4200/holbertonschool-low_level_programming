7ffedfb6a7148775476215a700a9d64230b88bfd
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-03-13 14:46:39 -0500

    Update 2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -7,33 +7,72 @@
  * Return: concat of s1 and s2
  */
 char *str_concat(char *s1, char *s2)
+		
 {
-char *conct;
-int i, ci;
-if (s1 == NULL || s2 == NULL)
-{
-s1 = "";
-s2 = "";
+		
+	char *conct;
+		
+	int i, ci;
+		
+
+		
+	if (s1 == NULL)
+		
+		s1 = "";
+		
+	if (s2 == NULL)
+		
+		s2 = "";
+		
+
+		
+		i = ci = 0;
+		
+	while (s1[i] != '\0')
+		
+		i++;
+		
+	while (s2[ci] != '\0')
+		
+		ci++;
+		
+	conct = malloc(sizeof(char) * (i + ci + 1));
+		
+
+		
+	if (conct == NULL)
+		
+		return (NULL);
+		
+	i = ci = 0;
+		
+	while (s1[i] != '\0')
+		
+	{
+		
+		conct[i] = s1[i];
+		
+		i++;
+		
+	}
+		
+
+		
+	while (s2[ci] != '\0')
+		
+	{
+		
+		conct[i] = s2[ci];
+		
+		i++, ci++;
+		
+	}
+		
+	conct[i] = '\0';
+		
+	return (conct);
+		
 }
-i = ci = 0;
-while (s1[i] != '\0' && s2[ci] != '\0')
-{
-i++;
-ci++;
-}
-conct = malloc(sizeof(char) * (i + ci + 1));
-if (conct == NULL)
-return (NULL);
-i = ci = 0;
-for (i = 0; s1[i] != '\0'; i++)
-{
-conct[i] = s1[i];
-}
-for (ci = 0; s2[ci] != '\0'; ci++)
-{
-conct[i] = s2[ci];
-i++;
-}
-conct[i] = '\0';
-return (conct);
-}
+
+
+
