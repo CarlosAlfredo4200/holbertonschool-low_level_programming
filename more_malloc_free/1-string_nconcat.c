d2b54f0a42ab3a382896e83feff77e30de5f2aa1
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-03-14 13:58:24 -0500

    Update 1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,17 +1,17 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * *string_nconcat - concatenates n bytes of a string to another string
- * @s1: string to append to
- * @s2: string to concatenate from
- * @n: number of bytes from s2 to concatenate to s1
- *
- * Return: pointer to the resulting string
- */
+* *string_nconcat - concatenates n bytes of a string to another string
+* @s1: string to append to
+* @s2: string to concatenate from
+* @n: number of bytes from s2 to concatenate to s1
+*
+* Return: pointer to the resulting string
+*/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *newCadena;
-unsigned int i, k, size_S1 = 0, size_S2 = 0;
+unsigned int i, size_S1 = 0, size_S2 = 0;
 if (s1 == 0)
 s1 = "";
 if (s2 == 0)
@@ -21,7 +21,7 @@ for (size_S1 = 0; s1[size_S1] != '\0'; size_S1++)
 for (size_S2 = 0; s2[size_S2] != '\0'; size_S2++)
 ;
 size_S2++;
-if (n < size_S2)
+if (n > size_S2)
 {
 n = size_S2;
 }
@@ -32,10 +32,9 @@ for ( i = 0; i < size_S1; i++)
 {
 newCadena[i] = s1[i];
 }
-for ( k = 0; k < (size_S1 + size_S2 - n + 1); k++)
+for(; i < (size_S1 + n); i++)
 {
-newCadena[i] = s2[k];
-i++;
+newCadena[i] = s2[i - size_S1];
 }
 newCadena[i] = '\0';
 return (newCadena);
