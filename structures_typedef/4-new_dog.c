23c5a6337b9f899737ecb5977431d254213cc0e7
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-03-20 17:51:11 -0500

    Update 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -8,28 +8,47 @@
  * @owner: string from main, owner of pet
  * Return: p
  */
-dog_t *new_dog(char *name, float age, char *owner)
+og_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p;
-	/* reserving memory to struct*/
-	p = malloc(sizeof(dog_t));
-	if (p == NULL)
-		return (NULL);
-	/* Cpunting name pointer*/
-	if (name == NULL)
-	{
-		free(p);
-		free(owner);
-		return (NULL);
-	}
-	if (owner == NULL)
-	{
-		free(p);
-		free(name);
-		return (NULL);
-	}
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
-	return (p);
+    struct dog *newDog_t;
+    int sizeName = 0;
+    int sizeOwner = 0;
+    int k = 0, i = 0;
+
+    for (k = 0; name[k] != '\0'; k++)
+    {
+        sizeName++;
+    }
+    for (i = 0; owner[i] != '\0'; i++)
+    {
+        sizeOwner++;
+    }
+
+    printf("%d -- %d\n", k, i);
+    newDog_t = malloc(sizeof(dog_t));
+    if (newDog_t == NULL)
+        return (NULL);
+
+    newDog_t->name = malloc(sizeof(char) * (sizeName + 1) );
+    if (newDog_t->name == NULL)
+    {
+        free(newDog_t);
+        return (NULL);
+    }
+
+    for (k = 0; k < sizeName; k++)
+        newDog_t->name[k] = name[k];
+
+    newDog_t->age = age;
+
+    newDog_t->owner = malloc(sizeof(char) * (sizeOwner + 1));
+    if (newDog_t->owner == NULL)
+    {
+        free(newDog_t->name);
+        free(newDog_t);
+        return (NULL);
+    }
+    for (k = 0; k < sizeOwner; k++)
+        newDog_t->owner[k] = owner[k];
+    return (newDog_t);
 }
