77c380d6cb4751514fd892c11e07d70e554aa703
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-03-20 15:55:59 -0500

    Update 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,37 +9,42 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *newDog_t;
+    struct dog *newDog_t;
     int sizeName = 0;
     int sizeOwner = 0;
-    int k, i;
+    int k = 0, i = 0;
 
     for (k = 0; name[k] != '\0'; k++)
-         {
-             sizeName++;
-         }
+    {
+        sizeName++;
+    }
     for (i = 0; owner[i] != '\0'; i++)
     {
-             sizeOwner++;
+        sizeOwner++;
     }
-
-    
 
     newDog_t = malloc(sizeof(dog_t));
     if (newDog_t == NULL)
         return (NULL);
-    newDog_t->name = malloc(sizeName * sizeof(newDog_t->name));
-    if (newDog_t == NULL)
+
+    newDog_t->name = malloc(sizeof(char) * (sizeName + 1) );
+    if (newDog_t->name == NULL)
+    {
+        free(newDog_t);
         return (NULL);
+    }
 
     for (k = 0; k < sizeName; k++)
         newDog_t->name[k] = name[k];
 
     newDog_t->age = age;
 
-    newDog_t->owner = malloc(sizeOwner * sizeof(newDog_t->owner));
-    if (newDog_t == NULL)
+    newDog_t->owner = malloc(sizeof(char) * (sizeOwner + 1));
+    if (newDog_t->owner == NULL)
+    {
+        free(newDog_t);
         return (NULL);
+    }
     for (k = 0; k < sizeOwner; k++)
         newDog_t->owner[k] = owner[k];
     return (newDog_t);
