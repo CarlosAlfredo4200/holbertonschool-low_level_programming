b4fc6ff651ce45cc45c7a2a41c54f0e176004f7b
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-03-22 09:54:50 -0500

    Update 2-int_index.c

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -8,14 +8,20 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-int i;
+int k;
 if (!(array) || size <= 0 || !(cmp))
 {
 return (-1);
 }
-for (i = 0; i < size; i++)
+else
 {
-cmp(array[i]);
+for (k = 0; k < size; k++)
+{
+if (cmp(array[k]))
+{
+return (k);
+};
 }
-return (i);
+return (-1);
+}
 }
