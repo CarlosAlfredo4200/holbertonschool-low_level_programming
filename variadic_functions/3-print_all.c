40a3bc8be1791f3014f26fb08c5284b5937febc3
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-03-23 13:40:29 -0500

    Update 3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -9,43 +9,43 @@
  */
 void print_all(const char * const format, ...)
 {
-  va_list parametros;
-  int n = 0, i = 0;
-  char *separator = ", ";
-  char *cadena;
+	va_list parametros;
+	int n = 0, i = 0;
+	char *separator = ", ";
+	char *cadena;
 
-  va_start(parametros, format);
+	va_start(parametros, format);
 
-  while (format && format[i])
-    i++;
-  while (format && format[n])
-    {
-      if (n  == (i - 1))
+	while (format && format[i])
+	i++;
+    while (format && format[n])
 	{
-	  separator = "";
+	if (n  == (i - 1))
+	{
+	separator = "";
 	}
-      switch (format[n])
+	switch (format[n])
 	{
 	case 'c':
-	  printf("%c%s", va_arg(parametros, int), separator);
-	  break;
+	printf("%c%s", va_arg(parametros, int), separator);
+	break;
 	case 'i':
-	  printf("%d%s", va_arg(parametros, int), separator);
-	  break;
+	printf("%d%s", va_arg(parametros, int), separator);
+	break;
 	case 'f':
-	  printf("%f%s", va_arg(parametros, double), separator);
-	  break;
+	printf("%f%s", va_arg(parametros, double), separator);
+	break;
 	case 's':
-	  cadena = va_arg(parametros, char *);
-	  if (cadena == NULL)
-	    {
-	      cadena = "(nil)";
-	    }
-	  printf("%s%s", cadena, separator);
-	  break;
+	cadena = va_arg(parametros, char *);
+	if (cadena == NULL)
+    {
+    cadena = "(nil)";
+    }	
+	printf("%s%s", cadena, separator);
+	break;
 	}
-      n++;
-    }
-  printf("\n");
-  va_end(parametros);
+	n++;
+	}
+	printf("\n");
+	va_end(parametros);
 }
