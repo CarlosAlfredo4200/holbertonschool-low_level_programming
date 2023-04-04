25c258ad646f39bfee660f14459d993278a00a25
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-04 15:34:56 -0500

    Update 0-print_dlistint.c

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -3,19 +3,19 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * add_node_end - prints all the elements of a list_t list.
- * @head: pointer to the first element of the list.
- * @str: char.
- * Return: number of nodes.
- **/
+* add_node_end - prints all the elements of a list_t list.
+* @head: pointer to the first element of the list.
+* @str: char.
+* Return: number of nodes.
+**/
 size_t print_dlistint(const dlistint_t *h)
 {
-  size_t count = 0;
-  while (h != NULL)
-    {
-      printf("%d\n", h->n);
-      h = h->next;
-      count++;
-    }
-  return (count);
+size_t count = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+count++;
+}
+return (count);
 }
