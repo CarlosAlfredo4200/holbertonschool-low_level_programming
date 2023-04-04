2a12205ee984d99289474367a6dba4c96c784e5c
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-04 13:02:43 -0500

    Update 0-print_dlistint.c

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -8,7 +8,8 @@
  * @str: char.
  * Return: number of nodes.
  **/
-size_t print_dlistint(const dlistint_t *h){
+size_t print_dlistint(const dlistint_t *h)
+{
   size_t count = 0;
   while (h != NULL)
     {
