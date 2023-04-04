efff855074d6b3d0fbac9127cd053ea4393a049e
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-04 15:38:58 -0500

    Update 0-print_dlistint.c

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 /**
-* print_dlistint - a function that prints all the elements of a dlistint_t
-* @s: dlistint_t.
+* print_dlistint - a function that prints all the elements of a
+* @h: head.
 * Return: number of nodes.
 **/
 size_t print_dlistint(const dlistint_t *h)
