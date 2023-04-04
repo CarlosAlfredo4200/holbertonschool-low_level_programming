94ad50a1acedfbc0d4b329e993963e2afa5802b7
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-04 15:37:02 -0500

    Update 0-print_dlistint.c

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -3,9 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 /**
-* add_node_end - prints all the elements of a list_t list.
-* @head: pointer to the first element of the list.
-* @str: char.
+* print_dlistint - a function that prints all the elements of a dlistint_t
+* @s: dlistint_t.
 * Return: number of nodes.
 **/
 size_t print_dlistint(const dlistint_t *h)
