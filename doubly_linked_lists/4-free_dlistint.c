913af2fc70501fcfece19cd19910324faea878cd
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-05 05:05:09 -0500

    Update 4-free_dlistint.c

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -13,8 +13,8 @@ void free_dlistint(dlistint_t *head)
 
   while (head)
     {
-      temp = head->next;
-      free(head);
-      head = temp;
-    }
+temp = head->next;
+free(head);
+head = temp;
+}
 }
