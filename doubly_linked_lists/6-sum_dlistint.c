665f1e4c3802f9860a7d12a8cf87f408e02eb5af
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-05 10:18:39 -0500

    Update 6-sum_dlistint.c

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -9,14 +9,12 @@
  */
 int sum_dlistint(dlistint_t *head)
 {
-  int sum = 0;
-  dlistint_t *current = head;
-
-  while (current)
-    {
-      sum += current->n;
-      current = current->next;
-    }
-
-  return sum;
+int sum = 0;
+dlistint_t *current = head;
+while (current)
+{
+sum += current->n;
+current = current->next;
+}
+return sum;
 }
