d724638deff8a91aa6e1a0dd891db7bb75154a81
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-05 16:18:30 -0500

    Update 7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,13 +13,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 dlistint_t *new_node, *temp;
 unsigned int i;
-if (h == NULL) /* Check if head pointer is NULL */
+if (h == NULL) 
 return (NULL);
-new_node = malloc(sizeof(dlistint_t)); /* Allocate memory for new node */
+new_node = malloc(sizeof(dlistint_t));
 if (new_node == NULL)
 return (NULL);
 new_node->n = n;
-if (idx == 0) /* Insert at beginning of list */
+if (idx == 0)
 {
 new_node->prev = NULL;
 new_node->next = *h;
@@ -29,21 +29,20 @@ if (*h != NULL)
 return (new_node);
 }
 temp = *h;
-for (i = 0; temp != NULL && i < idx; i++) /* Traverse list to find node at index idx */
+for (i = 0; temp != NULL && i < idx; i++)
 temp = temp->next;
-if (temp == NULL && i == idx) /* Insert at end of list */
+if (temp == NULL && i == idx) 
 {
 free(new_node);
 return (add_dnodeint_end(h, n));
 }
-else if (temp != NULL) /* Insert in middle of list */
-{
+else if (temp != NULL)
 new_node->prev = temp->prev;
 new_node->next = temp;
 temp->prev->next = new_node;
 temp->prev = new_node;
 return (new_node);
 }
-free(new_node); /* Insert at non-existent index */
+free(new_node);
 return (NULL);
 }
