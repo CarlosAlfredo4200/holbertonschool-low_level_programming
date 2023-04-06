76cf3629619ac7af6170d91df2446e4a1b58dc5b
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-06 09:13:35 -0500

    Update 0-binary_to_uint.c

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -10,13 +10,13 @@ unsigned int result = 0;
 unsigned int bit = 1;
 int i;
 if (b == NULL)
-return 0;
+return (0);
 for (i = strlen(b) - 1; i >= 0; i--)
 {
 if (b[i] == '1')
 result += bit;
 else if (b[i] != '0')
-return 0;
+return (0);
 bit <<= 1;
 }
 return (result);
