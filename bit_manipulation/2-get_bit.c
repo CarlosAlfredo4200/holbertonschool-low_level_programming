02df774d99a0323447d80ef40715a45af5d30c46
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-06 09:57:24 -0500

    Update 2-get_bit.c

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -6,10 +6,11 @@
  *
  * Return: bit at index
  */
-int get_bit(unsigned long int n, unsigned int index) {
-  if (index >= sizeof(unsigned long int) * 8) {
-    return -1;
-  }
-
-  return (n >> index) & 1;
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (index >= sizeof(unsigned long int) * 8)
+{
+return -1;
+}
+return (n >> index) & 1;
 }
