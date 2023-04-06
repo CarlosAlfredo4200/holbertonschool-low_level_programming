309a0ae4e4bb746a159b46028d97a4aeaafb6dc1
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-06 10:26:46 -0500

    Update 4-clear_bit.c

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -6,11 +6,12 @@
  *
  * Return: 1 if succeed -1 if failed
  */
-int clear_bit(unsigned long int *n, unsigned int index) {
-  if (index >= sizeof(unsigned long int) * 8) {
-    return -1;
-  }
-
-  *n &= ~(1UL << index);
-  return 1;
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+if (index >= sizeof(unsigned long int) * 8)
+{
+return -1;
+}
+*n &= ~(1UL << index);
+return 1;
 }
