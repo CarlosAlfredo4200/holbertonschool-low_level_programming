6db13a0f0b1c9065d7eff57f634393e7e2fc9972
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-06 10:57:54 -0500

    Update 100-get_endianness.c

--- bit_manipulation/100-get_endianness.c
+++ bit_manipulation/100-get_endianness.c
@@ -1,12 +1,10 @@
 #include "main.h"
 /**
- * flip_bits - gets the number of bits flipped form n to m
- * @n: initial lu int
- * @m: secondary lu int
- *
- * Return: number of bits need to flip to get from `n to `m`
+ * get_endianness - a function that checks the endianness
+ * Return: 0 if big endian, 1 if little endian
  */
-int get_endianness(void) {
+int get_endianness(void)
+{
 int i = 1;
 char *p = (char *)&i;
 return (*p == 1);
