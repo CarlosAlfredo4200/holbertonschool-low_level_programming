e41369dbe753a9fe267b2da5b8c0f5544b66e90f
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-10 13:55:17 -0500

    Update 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,8 +19,9 @@ int create_file(const char *filename, char *text_content)
 
     if (text_content == NULL)
     {
-        return (1);
-	    close(fnNewFile);
+        close(fnNewFile);
+	    return (1);
+	    
     }else
 	{
 		for (size = 0; text_content[size];)
