62a1cff90fb02844b4503022fe669a9fd74f62a3
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-10 13:48:18 -0500

    Update 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -22,10 +22,7 @@ int create_file(const char *filename, char *text_content)
 	for (size = 0; text_content[size];)
 	  size++;
       }
-	else
-    {
-        return (1);
-    }
+
 
     fnNewFile = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
     w = write(fnNewFile, text_content, size);
