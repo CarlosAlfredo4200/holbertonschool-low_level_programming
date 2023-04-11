8e6437e7137a077b718acf28721ffdadbc0a3fdb
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-11 11:49:44 -0500

    Update 1-djb2.c

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,16 +1,17 @@
 #include "hash_tables.h"
 /**
- *  * hash_djb2 - Hash function, takes a string and turns it into a hash number
- *   * @str: the string, a constant value unrepeated in the hash table
- *    *
- *     * Return: The hash number
- *      */
-unsigned long int hash_djb2(const unsigned char *str) {
-    unsigned long int hash = 5381;
-    int c;
-	while ((c = *str++) != 0)
-	{
-	hash = ((hash << 5) + hash) + c;
-    }
-    return hash;
+* hash_djb2 - Hash function, takes a string and turns it into a hash number
+* @str: the string, a constant value unrepeated in the hash table
+*
+* Return: The hash number
+ */
+unsigned long int hash_djb2(const unsigned char *str) 
+{
+unsigned long int hash = 5381;
+int c;
+while ((c = *str++) != 0)
+{
+hash = ((hash << 5) + hash) + c;
+}
+return hash;
 }
