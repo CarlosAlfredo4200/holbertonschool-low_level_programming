0c2e482e8ccfd04c4b6a266062c85e654473f637
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-11 11:32:52 -0500

    Update 1-djb2.c

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -5,12 +5,12 @@
  *    *
  *     * Return: The hash number
  *      */
-unsigned long int hash_djb2(const unsigned char *str)
-{
-unsigned long int hash = 5381;
-int c;
-while ((c = *str++) != 0)
-		{
-				hash = ((hash << 5) + hash) + c;
+unsigned long int hash_djb2(const unsigned char *str) {
+    unsigned long int hash = 5381;
+    int c;
+	while ((c = *str++) != 0)
+	{
+	hash = ((hash << 5) + hash) + c;
+    }
+    return hash;
 }
-return (hash);
