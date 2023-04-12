1ff5b8122ef622acb1064ade4ac75106f0fc2e2b
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-12 08:51:58 -0500

    Update 0-hash_table_create.c

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -8,22 +8,26 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-unsigned long int i;
-hash_table_t *hash_table;
-if (size <= 0)
-return (NULL);
-hash_table = NULL;
-hash_table = malloc(sizeof(hash_table_t));
-if (hash_table == NULL)
-return (NULL);
-hash_table->size = size;
-hash_table->array = malloc(sizeof(hash_node_t *) * size);
-if (hash_table->array == NULL)
-{
-free(hash_table);
-return (NULL);
-}
-for (i = 0; i < size; i++)
-hash_table->array[i] = NULL;
-return (hash_table);
+    hash_table_t *ht;
+    unsigned long int i;
+    ht = malloc(sizeof(hash_table_t));
+    if (ht == NULL)
+    {
+        return (NULL);
+    }
+
+    ht->size = size;
+    ht->array = malloc(size * sizeof(hash_node_t *));
+    if (ht->array == NULL)
+    {
+        free(ht);
+        return (NULL);
+    }
+
+    for (i = 0; i < size; i++)
+    {
+        ht->array[i] = NULL;
+    }
+
+    return (ht);
 }
