c7d2d5a9c47fda929cf29d1085db81498643b1b0
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-12 08:06:05 -0500

    Update 3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -5,54 +5,52 @@
 #include <stdlib.h>
 
 
-// Función para crear una nueva tabla hash
-hash_table_t* create_hash_table(int size) {
-    hash_table_t *ht = (hash_table_t*) malloc(sizeof(hash_table_t));
-    ht->size = size;
-    ht->buckets = (hash_node_t**) calloc(size, sizeof(hash_node_t*));
-    return ht;
-}
-
-// Función para calcular el hash de una cadena de caracteres
-unsigned long hash(const char *str, int size) {
-    unsigned long hash = 5381;
+unsigned long int hash_djb2(const unsigned char *str)
+{
+    unsigned long int hash;
     int c;
+    hash = 5381;
     while ((c = *str++))
-        hash = ((hash << 5) + hash) + c;
-    return hash % size;
+    {
+        hash = ((hash << 5) + hash) + (c);
+    }
+    return (hash);
 }
 
-// Función para crear un nuevo nodo de la tabla hash
-hash_node_t* create_hash_node(const char *key, const char *value) {
-    hash_node_t *node = (hash_node_t*) malloc(sizeof(hash_node_t));
-    node->key = strdup(key);
-    node->value = strdup(value);
-    node->next = NULL;
-    return node;
-}
-
-// Función para agregar o actualizar un elemento en la tabla hash
-int hash_table_set(hash_table_t *ht, const char *key, const char *value) {
-    if (ht == NULL || key == NULL || strlen(key) == 0) {
-        return 0; // No se permite tabla hash nula, clave nula o clave vacía
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+    if (ht == NULL || key == NULL || *key == '\0')
+    {
+        return 0; // Si la tabla hash o la clave son nulas o la clave está vacía, retornar 0 (fallo)
     }
 
-    unsigned long index = hash(key, ht->size);
-    hash_node_t *node = ht->buckets[index];
+    unsigned long int index = hash_djb2((const unsigned char *)key) % ht->size; // Calcular el índice en la tabla hash
 
-    // Buscar si la clave ya existe en la tabla hash
-    while (node != NULL) {
-        if (strcmp(node->key, key) == 0) {
-            free(node->value); // Liberar la memoria del valor existente
-            node->value = strdup(value); // Duplicar y actualizar el valor
-            return 1; // Se ha actualizado el valor existente
+    hash_node_t *node = ht->array[index]; // Obtener el nodo en la posición del índice
+
+    while (node != NULL)
+    {
+        if (strcmp(node->key, key) == 0)
+        {
+            // Si la clave ya existe, actualizar el valor y retornar 1 (éxito)
+            free(node->value); // Liberar el valor anterior
+            node->value = strdup(value); // Duplicar y asignar el nuevo valor
+            return 1;
         }
-        node = node->next;
+        node = node->next; // Avanzar al siguiente nodo en caso de colisión
     }
 
-    // La clave no existe, crear un nuevo nodo y agregarlo a la tabla hash
-    hash_node_t *new_node = create_hash_node(key, value);
-    new_node->next = ht->buckets[index];
-    ht->buckets[index] = new_node;
-    return 1; // Se ha agregado un nuevo elemento a la tabla hash
+    // Si la clave no existe, crear un nuevo nodo y agregarlo al principio de la lista
+    hash_node_t *new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
+    if (new_node == NULL)
+    {
+        return 0; // Si no se pudo asignar memoria, retornar 0 (fallo)
+    }
+
+    new_node->key = strdup(key);     // Duplicar y asignar la clave al nuevo nodo
+    new_node->value = strdup(value); // Duplicar y asignar el valor al nuevo nodo
+    new_node->next = ht->array[index]; // El siguiente nodo es el que estaba previamente en esa posición del índice
+    ht->array[index] = new_node; // El nuevo nodo se convierte en el primer nodo en esa posición del índice
+
+    return 1; // Retornar 1 (éxito)
 }
