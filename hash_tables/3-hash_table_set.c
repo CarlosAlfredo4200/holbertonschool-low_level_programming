45ce7aefc74f28d18d6bd8fba17df1ad8d729268
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-12 08:32:55 -0500

    Update 3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -5,41 +5,41 @@
 #include <stdlib.h>
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
-{   
-    unsigned long int index;
-    hash_node_t *new_node;
-    if (ht == NULL || key == NULL || strcmp(key, "") == 0)
     {
+        unsigned long int index;
+        hash_node_t *new_node;
+    if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
         return 0;
-    }
 
     index = hash_djb2((const unsigned char *)key) % ht->size;
-
     new_node = malloc(sizeof(hash_node_t));
     if (new_node == NULL)
-    {
         return 0;
-    }
 
-    new_node->key = strdup(key);
-    if (new_node->key == NULL)
-    {
+    char *key_dup = strdup(key);
+    if (key_dup == NULL) {
         free(new_node);
         return 0;
     }
 
-    new_node->value = strdup(value);
-    if (new_node->value == NULL)
-    {
-        free(new_node->key);
+    char *value_dup = strdup(value);
+    if (value_dup == NULL) {
+        free(key_dup);
         free(new_node);
         return 0;
     }
 
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
+    new_node->key = key_dup;
+    new_node->value = value_dup;
+    new_node->next = NULL;
+
+    if (ht->array[index] == NULL) {
+        ht->array[index] = new_node;
+    } else {
+        
+        new_node->next = ht->array[index];
+        ht->array[index] = new_node;
+    }
 
     return 1;
 }
-
- 
