4c38cc270b9f7b87d0331bd9b0a27550ab9b5d9f
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-12 08:42:43 -0500

    Update 3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -6,14 +6,13 @@
 
 unsigned long int hash_djb2(const unsigned char *str)
 {
-unsigned long int hash;
-int c;
-hash = 5381;
-while ((c = *str++))
-{
-hash = ((hash << 5) + hash)+(c);
-}
-return (hash);
+    unsigned long int hash = 5381;
+    int c;
+    while ((c = *str++))
+    {
+        hash = ((hash << 5) + hash) + c;
+    }
+    return hash;
 }
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
