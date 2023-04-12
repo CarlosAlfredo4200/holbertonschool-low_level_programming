efb22e1d8e90ec80acbcf3fa1601ccbca33b64e9
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-12 08:48:40 -0500

    Update 3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -6,7 +6,9 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht = malloc(sizeof(hash_table_t));
+    hash_table_t *ht;
+    unsigned long int i;
+    ht = malloc(sizeof(hash_table_t));
     if (ht == NULL)
     {
         return NULL;
@@ -20,7 +22,7 @@ hash_table_t *hash_table_create(unsigned long int size)
         return NULL;
     }
 
-    for (unsigned long int i = 0; i < size; i++)
+    for (i = 0; i < size; i++)
     {
         ht->array[i] = NULL;
     }
