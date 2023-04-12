1e8353f6f4d55e275e7d22f353c0529e7d0bd974
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-12 09:59:21 -0500

    Update 5-hash_table_print.c

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -8,26 +8,28 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-  unsigned long int i;
-  hash_node_t *node;
-  if (ht == NULL)
-    return;
-
-  printf("{");
-  for (i = 0; i < ht->size; i++) {
-    node = ht->array[i];
-
-    while (node != NULL) { 
-      printf("'%s': '%s'", node->key, node->value);
-
-      if (node->next != NULL)
-	printf(", ");
-
-      node = node->next;
+	unsigned long int i;
+	hash_node_t *node;
+    if (ht == NULL)
+    {
+        return;
     }
 
-    if (i < ht->size - 1 && ht->array[i + 1] != NULL)
-      printf(", ");
-  }
-  printf("}\n");
+    printf("{");
+
+
+    for (i = 0; i < ht->size; i++)
+    {
+        node = ht->array[i];
+        while (node != NULL)
+        {
+            printf("'%s': '%s'", node->key, node->value);
+            if (node->next != NULL)
+            {
+                printf(", ");
+            }
+            node = node->next;t
+        }
+    }
+    printf("}\n");
 }
