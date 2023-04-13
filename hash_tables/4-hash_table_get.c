fc100ea366d7331b71d002dc9294e7187c3a1f81
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-13 09:30:49 -0500

    Update 4-hash_table_get.c

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -7,26 +7,19 @@
  *
  * Return: The value if works, NULL if doesn't.
  */
-
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-  unsigned long int index;
-  hash_node_t *node;
-  if (ht == NULL || key == NULL)
-    return NULL;
-
-
-  index = hash_djb2((const unsigned char *)key) % ht->size;
-
-
-  node = ht->array[index];
-  while (node != NULL)
-    {
-      if (strcmp(node->key, key) == 0)
-	return node->value;
-
-      node = node->next;
-    }
-
-  return NULL;
+unsigned long int index;
+hash_node_t *node;
+if (ht == NULL || key == NULL)
+return NULL;
+index = hash_djb2((const unsigned char *)key) % ht->size;
+node = ht->array[index];
+while (node != NULL)
+{
+if (strcmp(node->key, key) == 0)
+return node->value;
+node = node->next;
+}
+return NULL;
 }
