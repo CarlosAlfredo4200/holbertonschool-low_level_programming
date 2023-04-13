bd92824335608cf5bf571a103f802b7c25d59b0b
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-13 10:39:52 -0500

    Update 6-hash_table_delete.c

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -8,23 +8,23 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-  unsigned long int i;
-  hash_node_t *current;
-  hash_node_t *next;
-  if (ht == NULL) {
-    return;
-  }
-
-  for (i = 0; i < ht->size; i++) {
-    current = ht->array[i];
-    while (current != NULL) {
-      next = current->next;
-      free(current->key);
-      free(current->value);
-      free(current);
-      current = next;
-    }
-  }
-  free(ht->array);
-  free(ht);
+unsigned long int i;
+hash_node_t *current;
+hash_node_t *next;
+if (ht == NULL) {
+return;
+}
+for (i = 0; i < ht->size; i++) 
+{
+current = ht->array[i];
+while (current != NULL) {
+next = current->next;
+free(current->key);
+free(current->value);
+free(current);
+current = next;
+}
+}
+free(ht->array);
+free(ht);
 }
