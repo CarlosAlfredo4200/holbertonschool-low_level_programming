5eae5807787e54e0ba0c443134d6aea2b414a2f8
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-04-13 09:36:33 -0500

    Update 4-hash_table_get.c

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -12,14 +12,14 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 unsigned long int index;
 hash_node_t *node;
 if (ht == NULL || key == NULL)
-return NULL;
+return (NULL);
 index = hash_djb2((const unsigned char *)key) % ht->size;
 node = ht->array[index];
 while (node != NULL)
 {
 if (strcmp(node->key, key) == 0)
-return node->value;
+return (node->value);
 node = node->next;
 }
-return NULL;
+return (NULL);
 }
